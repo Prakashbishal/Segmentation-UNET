a2af1a1d6145d745cd7d72d46128d1011d99fc81
Bishal <prakashbishal1973@gmail.com>
2025-12-19 23:28:53 +0000

    Initial Commit

---
+++ src/download_and_view_pet.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+from torchvision.datasets import OxfordIIITPet
+from torchvision.transforms import ToTensor
+from pathlib import Path
+
+root = Path("data")
+
+# target_types="segmentation" gives you pixel mask labels
+ds = OxfordIIITPet(
+    root=root,
+    split="trainval",
+    target_types="segmentation",
+    download=True,
+)
+
+img, mask = ds[0]
+
+plt.figure(figsize=(8, 4))
+plt.subplot(1, 2, 1)
+plt.title("Image")
+plt.imshow(img)
+plt.axis("off")
+
+plt.subplot(1, 2, 2)
+plt.title("Mask")
+plt.imshow(mask)
+plt.axis("off")
+
+plt.tight_layout()
+plt.show()
---
+++ src/sanity_check.py
@@ -0,0 +1,5 @@
+import torch
+import torchvision
+print("Torch:", torch.__version__)
+print("Torchvision:", torchvision.__version__)
+print("CUDA available:", torch.cuda.is_available())
